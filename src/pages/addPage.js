a828f89d81a432383b7f0267d727b4a1847f3a91
sustvero <vsustrov@uwaterloo.ca>
2022-08-27 00:29:23 -0400

    Added initial routing, pages, and navbar

---
+++ src/pages/addPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/navbar';
+
+const AddPage = () => {
+  return (
+    <div>
+      <Navbar />
+      My Recipes
+    </div>
+  )
+}
+
+export default AddPage
---
+++ src/pages/recipes.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/navbar';
+
+const Recipes = () => {
+  return (
+    <div>
+        <Navbar />
+        Recipes
+    </div>
+  )
+}
+
+export default Recipes
